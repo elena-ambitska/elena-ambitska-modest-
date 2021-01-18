279709b099f20259788ad2bb6caf3dd0639a991e
Lena <lambitska92@gmail.com>
2021-01-19 01:21:51 +0200

    Added contact form

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 //mobile menu
-let mobileMenu= document.querySelector(".nav-mobile-menu");
-let mainMenu=document.querySelector(".menu");
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".menu");
 
-mobileMenu.addEventListener("click",function(){
+mobileMenu.addEventListener("click", function () {
     mobileMenu.classList.toggle("active-menu");
-    if(mobileMenu.classList.contains("active-menu")){
-mainMenu.classList.add("active-menu");
-}else{
-    mainMenu.classList.remove("active-menu");
-}
+    if (mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
 })
+
+//contact-form
+
+let openForm=document.querySelector(".open-form");
+let contactForm=document.querySelector(".contact-form");
+
+openForm.addEventListener("click", function(){
+ 
+    if (!contactForm.classList.contains("show")) {
+        contactForm.classList.add("show");
+        contactForm.classList.remove("hide");
+        openForm.classList.add("hide");
+    } else {
+        contactForm.classList.remove("show");
+    }
+})  
